18f3e4b8a1bc3854aa2cd14c73d772e64929369f
OmarAlareeki <omarproductions2018@gmail.com>
2021-09-16 15:31:26 -0700

    reached -> square displays X on click

--- src/Board.js
+++ src/Board.js
@@ -1,33 +1,34 @@
 import React from 'react'
+import './App.css';
 import Square from './Square'
 
 
 const Board = () => {
-    function renderSquare(i) {
-        return <Square />;
+
+    const [squares, setSquares] = React.useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        let nextSquares = squares.slice();
+        nextSquares[i] = 'X'
+        setSquares(nextSquares)
     };
+
+    function renderSquare(i) {
+        return <Square 
+                value={squares[i]}
+                onClick={() => handleClick(i)}/>;
+    };
+
+    //const status = 'Next player: X';
 
     return (
         <div>
             <div className="status"></div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
+            <div className="board-row">{renderSquare(0)}{renderSquare(1)}{renderSquare(2)}</div>
+            <div className="board-row">{renderSquare(3)}{renderSquare(4)}{renderSquare(5)}</div>
+            <div className="board-row">{renderSquare(6)}{renderSquare(7)}{renderSquare(8)}</div>
         </div>
     );
 };
-
 
 export default Board
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './App.css';
 import Board from './Board'
 
 const Game = () => {
     return (
-            <div className="game">
+            <div className="game">             
                 <div className="game-board">
                     <Board />
                 </div>
